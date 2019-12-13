835a9aa685dda8cddba1ab35f7f267fa68ceafad
Akadar.akku <akadar.akku121@gmail.com>
2019-12-13 23:07:02 +0100

    This is the node-js week1 project

--- functions/find-by/id/index.js
+++ functions/find-by/id/index.js
@@ -1,5 +1,8 @@
 const id = (pokeData, id) => {
-
+    if (pokeData.find(element => element.id === id)) {
+        return pokeData.find(element => element.id === id);
+      } else {
+        return {};
+      }
 }
-
 module.exports = id;
--- functions/find-by/key-value/index.js
+++ functions/find-by/key-value/index.js
@@ -3,8 +3,15 @@
 // (later you will learn about req.body to pass any type)
 
 const keyValue = (pokeArray, key, value) => {
+    let keyValueObject = pokeArray.find(item => item[key] == value);
+    if (!keyValueObject) return[];
+    
+    const resultArrayFull = pokeArray.filter(item => item[key] == value);
 
+    const resultArrayExpected = [];
+    resultArrayFull.forEach(item => {
+        resultArrayExpected.push({"num": item.num, "name": item.name})
+    });
+    return resultArrayExpected;
 };
-
-
 module.exports = keyValue;
