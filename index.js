b68108dc15d82086a3ab2714117c2619f7bec811
Akadar.akku <akadar.akku121@gmail.com>
2019-12-14 20:05:20 +0100

    find-by-type branch

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// "value" will always be a string, because params always are!
-// consider using loose equality for your comparison
-// (later you will learn about req.body to pass any type)
-
-const keyValue = (pokeArray, key, value) => {
-    let pokeArr = [];
-    let filteredArr = pokeArray.filter(item => item[key] == value);
-    if (filteredArr.length !== 0) {
-      filteredArr.map(element => {
-        let pokeObj = {};
-        pokeObj.num = element.num;
-        pokeObj.name = element.name;
-        pokeArr.push(pokeObj);
-      });
-      return pokeArr;
-    } else {
-      return pokeArr;
-    }
+const type = (pokeArray, type) => {
+    let filteredArr = pokeArray.filter(item => item.type.includes(type));
+    filteredArr = filteredArr.map(element => ({
+      name: element.name,
+      num: element.num
+    }));
+    return filteredArr;
   };
   
-  module.exports = keyValue;
+  module.exports = type;
